18e7ae56444e0172c7f943f26c474eccbecb1af1
bogdandiaconupv <bogdan.diaconupv@gmail.com>
2023-03-23 18:25:35 +0200

    final touch mapping

--- frontend/src/Components/Locations.jsx
+++ frontend/src/Components/Locations.jsx
@@ -1,6 +1,6 @@
 export default function Locations(props) {
     
     return (
-        <button onClick={props.locationHandler} id={props.id}>{props.location.name}</button>
+        <button className="locBtn" onClick={props.locationHandler} id={"loc-"+props.id}>{props.location.name}</button>
     )
 }
