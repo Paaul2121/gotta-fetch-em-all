6a8c872613487e0afebf35505795b5ed29937275
Oliver <olialexander08@gmail.com>
2023-03-24 18:37:21 +0200

    finished selecting pokemons

--- frontend/src/Components/AllPokemons.jsx
+++ frontend/src/Components/AllPokemons.jsx
@@ -7,6 +7,8 @@ export default function AllPokemons() {
     const [loading, setLoadig] = useState(false)
     const [counter, setCounter] = useState(1)
     const [filterInput, setFilterInput] = useState("")
+    const [selectedPokemons, setSelectPokemons] = useState([])
+    const [maxSelectedPokemons, setMaxSelectedPokemons] = useState(0);
 
     useEffect(() => {
         const loader = async () => {
@@ -20,9 +22,8 @@ export default function AllPokemons() {
             }
         };
         //649
-        if (counter <= 100) {
+        if (counter <= 20) {
             loader();
-            console.log(allpokemons)
         }else{
             setLoadig(true)
         }
@@ -36,6 +37,28 @@ export default function AllPokemons() {
         setFilterInput(e.target.value);
     }
 
+    const pokemonCardEvent = (e) =>{
+        { selectedPokemons.length == 3 && console.log(selectedPokemons);} 
+
+        if(!e.target.nextSibling.classList.value.includes('selectedPokemon')){
+
+            if(maxSelectedPokemons < 3){
+                // e.target.nextSibling.classList.includes('selectedPokemon')
+            setSelectPokemons( prev => [...prev,JSON.parse(e.target.id) ] );
+            setMaxSelectedPokemons( maxSelectedPokemons + 1);
+            console.log(e.target.nextSibling)
+            e.target.nextSibling.classList.add("selectedPokemon")
+            }
+            
+        }else{
+            e.target.nextSibling.classList.remove("selectedPokemon")
+            console.log(JSON.parse(e.target.id).name)
+            setSelectPokemons((prev) => [...prev].filter( elem => elem.id != JSON.parse(e.target.id).id))
+            console.log(selectedPokemons)
+            setMaxSelectedPokemons( maxSelectedPokemons - 1)
+        }
+    }
+
     return (
         <div id="pokedexMenu">
             <div id="pokedexHeader">
@@ -46,17 +69,17 @@ export default function AllPokemons() {
 
             <div id="pokedex">
 
-            {filterInput == "" && loading && allpokemons?.map(pokemon =>
-                   <PokemonCard pokemon={pokemon} />
+            {filterInput == "" && loading && allpokemons?.map((pokemon,index) =>
+                   <PokemonCard key={index} pokemon={pokemon} pokemonCardEvent={pokemonCardEvent} />
                 )}
 {/* 
                 //loading pokemons by filter */}
 
                 {
-                    filterInput != "" && loading && allpokemons?.map(pokemon => {
+                    filterInput != "" && loading && allpokemons?.map((pokemon,index) => {
                         if (pokemon.name.includes(filterInput)) {
                             return (
-                                <PokemonCard pokemon={pokemon} />
+                                <PokemonCard key={index} pokemon={pokemon} />
                     )}
                     })
 
