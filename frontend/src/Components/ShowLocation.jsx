547f2500e99308b98734369d73319eff927b9003
Oliver <olialexander08@gmail.com>
2023-03-29 15:45:02 +0300

    fight video

--- frontend/src/Components/ShowLocation.jsx
+++ frontend/src/Components/ShowLocation.jsx
@@ -3,6 +3,7 @@ import {useAtom} from "jotai"
 import state from "./AtomStates"
 import WinOrLoseMSG from "./WinOrLoseMSG"
 import FriendlyPokemonCard from "./FriendlyPokemonCard"
+import FightIntro from "./FightIntro"
 
  const bloodImg = ["https://o.remove.bg/downloads/ec99d8e8-3c55-492f-84f5-be5bf33079a1/png-transparent-blood-blood-miscellaneous-image-file-formats-text-thumbnail-removebg-preview.png","https://o.remove.bg/downloads/6433ad2a-572c-4c79-a329-2f1f49809f25/png-transparent-blood-blood-miscellaneous-hand-photography-removebg-preview.png","https://o.remove.bg/downloads/5a976ed4-737b-46b7-9990-6f83965fb212/png-transparent-splash-of-blood-bloodstain-pattern-analysis-blood-love-miscellaneous-text-removebg-preview.png","https://o.remove.bg/downloads/5c968aa5-f88f-4b16-8d51-97ebb2bd5ff9/png-transparent-blood-splash-blood-splash-of-red-blood-miscellaneous-ink-color-splash-removebg-preview.png"];
 
@@ -15,8 +16,9 @@ export default function ShowLocation(props) {
     const [selectedPokemons,setSelectedPokemons] = useAtom(state.selectedPokemons)
     const [dead_Pokemons_Number, setDead_Pokemons_Number] = useState(0)
     const [friendlySelectedPokemon, setFriendlySelectedPokemon] = useState(null);
-    const [winOrLose, setWinOrLose] = useState(false)
+    const [winOrLose, setWinOrLose] = useState(null)
     const [deadEnemyPokemon, setDeadEnemyPokemon] = useState(false);
+    const [showFightVideo, setShowFightVideo] = useState(false)
     
 
     useEffect(() => {
@@ -89,10 +91,11 @@ export default function ShowLocation(props) {
          deadPokemon.nextSibling.classList.remove("back")
 
          setDead_Pokemons_Number( dead_Pokemons_Number + 1)
-         setWinOrLose(false)
+         
          console.log(dead_Pokemons_Number)
          if(dead_Pokemons_Number+1 == selectedPokemons.length){
-            document.getElementById("backToMapButton").style.visibility = "visible"
+            document.getElementById("backToMapButton").style.visibility = "visible";
+            setWinOrLose(false)
          }
         }
         
@@ -110,6 +113,11 @@ export default function ShowLocation(props) {
         setStartBattle(true)
         e.target.style.visibility = "hidden"
          document.getElementById("backToMapButton").style.visibility = "hidden"
+         
+         setShowFightVideo(true)
+         setInterval(()=>{
+            setShowFightVideo(false)
+         },2300);
         }
     }
 
@@ -118,6 +126,7 @@ export default function ShowLocation(props) {
 
     return (
         <div id="battleGround">
+
 
             {enemyPokemon && startBattle && dead_Pokemons_Number != selectedPokemons.length && !deadEnemyPokemon &&
             <>
@@ -144,9 +153,14 @@ export default function ShowLocation(props) {
                         <p>ATTACK : {friendlySelectedPokemon.stats[1].base_stat}</p>
                     </div>
             </div>}
-             
-             {dead_Pokemons_Number == selectedPokemons.length || deadEnemyPokemon &&
-             <WinOrLoseMSG classWinOrLose={winOrLose? "Win" : "Lose"}/>
+
+            
+             {dead_Pokemons_Number == selectedPokemons.length || deadEnemyPokemon ?
+             <WinOrLoseMSG classWinOrLose={winOrLose? "Win" : "Lose"}/> : null
+             }
+
+             {showFightVideo && 
+                <FightIntro/>
              }
 
             <button id="battleButton" onClick={startBattleEvt}>START BATTLE</button>
